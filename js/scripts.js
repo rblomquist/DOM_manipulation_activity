5fef07a10add465a778c7cb5dd60c71c96e8b020
rblomquist <rblomquist91@gmail.com>
2022-05-24 11:58:10 -0400

    added if statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,28 @@ const ulist = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
+
 button.addEventListener("click", () => {
-    const value = input.value;
-    input.value = "";
+    if(input.value != "") {
+        const value = input.value;
+        input.value = "";
 
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
-    const newButton = document.createElement("button");
+        const newLi = document.createElement("li");
+        const newSpan = document.createElement("span");
+        const newButton = document.createElement("button");
 
-    newLi.appendChild(newSpan);
-    newSpan.textContent = value;
-    
-    newLi.appendChild(newButton);
-    newButton.textContent = "❌";
+        newLi.appendChild(newSpan);
+        newSpan.textContent = value;
+        
+        newLi.appendChild(newButton);
+        newButton.textContent = "❌";
 
-    ulist.appendChild(newLi);
+        ulist.appendChild(newLi);
 
-    newButton.addEventListener('click', () => {
-        ulist.removeChild(newLi);
-    });
-
+        newButton.addEventListener('click', () => {
+            ulist.removeChild(newLi);
+        });
 
     input.focus();
+    }
 });
